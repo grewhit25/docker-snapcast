c521bf96d4aec1da051165a334ac36a8a878dfe8
Greg White <grewhit25@gmail.com>
2020-04-08 11:43:58 +0100

    switch to develpo

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# set defaults
+#export SPOTIFY_NAME="${SPOTIFY_NAME:=Snapcast}"
+#export SPOTIFY_BITRATE="${SPOTIFY_BITRATE:=160}"
+
+# setup dbus
+rm -rf /var/run/dbus
+mkdir -p /var/run/dbus
+chown -R messagebus:messagebus /var/run/dbus
+dbus-uuidgen --ensure
+dbus-daemon --system --fork
+sleep 3
+
+avahi-daemon -D
+
+exec "$@"
