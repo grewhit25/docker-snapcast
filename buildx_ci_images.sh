12c8c97de6d43f4ec37c5b0b55b879d5505e6227
Greg White <grewhit25@gmail.com>
2020-06-11 21:02:22 +0100

    alpine build source

--- buildx_ci_images.sh
+++ buildx_ci_images.sh
@@ -134,13 +134,13 @@ function build_ci_images::main() {
   #export DOCKER_BASE=${DOCKER_REGISTRY}/${TRAVIS_REPO_SLUG#*/}
   # Build server
   
-  export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapcast'
-  cp Dockerfile-Dsource Dockerfile.multi-arch
+  export DOCKER_BASE=${DOCKER_REGISTRY}'/alpine-snapcast'
+  cp Dockerfile-Asource Dockerfile.multi-arch
   build_ci_images::build_and_push_all
-  export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapserver'
-  cp Dockerfile-Dserver Dockerfile.multi-arch
-  build_ci_images::build_and_push_all
-  export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapclient'
-  cp Dockerfile-Dclient Dockerfile.multi-arch
-  build_ci_images::build_and_push_all
+  #export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapserver'
+  #cp Dockerfile-Dserver Dockerfile.multi-arch
+  #build_ci_images::build_and_push_all
+  #export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapclient'
+  #cp Dockerfile-Dclient Dockerfile.multi-arch
+  #build_ci_images::build_and_push_all
 }
