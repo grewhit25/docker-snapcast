639402d7cbe6c204a674dea8f36184061aacc979
Greg White <grewhit25@gmail.com>
2020-06-27 17:11:25 +0100

    add amd image

--- buildx_ci_images.sh
+++ buildx_ci_images.sh
@@ -130,16 +130,12 @@ function setup_ci_environment::main() {
 
 # Build images
 function build_ci_images::main() {
-  echo '** Next line need to move to env once dockerhub username updated **'
   #export DOCKER_BASE=${DOCKER_REGISTRY}/${TRAVIS_REPO_SLUG#*/}
   # Build server
   
-  export DOCKER_BASE=${DOCKER_REGISTRY}'/alpine-snapcast'
-  cp Dockerfile-Asource Dockerfile.multi-arch
-  build_ci_images::build_and_push_all
   #export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapserver'
-  #cp Dockerfile-Dserver Dockerfile.multi-arch
-  #build_ci_images::build_and_push_all
+  cp Dockerfile-Dserver-s6 Dockerfile.multi-arch
+  build_ci_images::build_and_push_all
   #export DOCKER_BASE=${DOCKER_REGISTRY}'/debian-snapclient'
   #cp Dockerfile-Dclient Dockerfile.multi-arch
   #build_ci_images::build_and_push_all
